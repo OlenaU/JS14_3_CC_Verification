a5ff1fa82fe0eee02abeaafa118ae73cd1019502
OlenaU <udalaolena@gmail.com>
2022-02-12 09:44:28 -0800

    working variant

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,24 @@
+//https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D0%B3%D0%BE%D1%80%D0%B8%D1%82%D0%BC_%D0%9B%D1%83%D0%BD%D0%B0#JavaScript
 //https://en.wikipedia.org/wiki/Payment_card_number
-
-
-let card = '3537791645445008';
-//2720990734838177
-
-const arrlength = [13, 14, 15, 16, 17, 18, 19]
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test#example_using_test
+//https://www.youtube.com/watch?v=_pLpx6btq6U  Regex
 
 function cardcheck(){
 
-  let card = document.getElementById('xvar').value.split("").reverse() .map(i => +i);
-  console.log(card);
+  let card = document.getElementById('xvar').value.split("");
+  let card1 = card.reverse().map(i => +i);
+  
+  console.log(card, card1);
+  
+
   let sum = 0;
-  for  (let i = 0; i< card.length; i++){
+  for  (let i = 0; i< card1.length; i++){
   if(i % 2 == 0) {
-      sum = sum + card[i];
-  } else {
-      let double = card[i] * 2;
+      sum = sum + card1[i];
+  } 
+  else 
+  {
+      let double = card1[i] * 2;
       sum = sum + (double > 9 ? double - 9 : double);
   }
   
@@ -23,49 +26,68 @@ function cardcheck(){
   let isCorrect = sum % 10 == 0;
   
   console.log(`Is Correct:`, isCorrect);
-}
 
 
-  let card = document.getElementById('xvar').value.split("")
-     
      let amex = /^3[47][0-9]{13}$/;
-     let visa =/^4[0-9]{12}(?:[0-9]{3})?$/;
+
+     let visa = /^4\d{3}(| |-)(?:\d{4}\1){2}\d{4}$/;  
    
-     //let mastercard = /(^5[1-5][0-9]{14}|^(222[1-9]|22[3-9]\\d|2[3-6]\\d{2}|27[0-1]\\d|2720)[0-9]{12}$/;
+     let mastercard = /^5[1-5][0-9]{14}|^(222[1-9]|22[3-9]\\d|2[3-6]\\d{2}|27[0-1]\\d|2720)[0-9]{12}$/;
      
-     let disco1 = /65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/;
-     
+     let disco = /^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$/;
      
      let diners = /^3[0689][0-9]{12}[0-9]*$/;
+     
      let jcb =  /^35[0-9]{14}[0-9]*$/;
-   
-    
-     var x = document.getElementById("xvar").value;
 
-  if(amex.test(x))
+     let maestro = /^(5018|5020|5038|6304|6759|6761|6763)[0-9]{8,15}$/;
+                
+     var x = document.getElementById('xvar').value
+    
+     if (visa.test(x)) 
         {
-            window.alert(`${x} is a valid American Express number`);
-            
+            window.alert(`${x} is a valid Visa card number`);
         }
-     else if (visa.test (x)) 
+       else if(amex.test(x))
         {
-            window.alert(`${x} is a valid Visa number`);
+            window.alert(`${x} is a valid American Express card number`);
+          
+        }
+        else if( mastercard.test(x))
+        {
+            window.alert(`${x} is a valid Mastercard number`);
+          
+        }
+        else if( disco.test(x))
+        {
+            window.alert(`${x} is a valid Discover card number`);
+          
+        }
+        else if( diners.test(x))
+        {
+            window.alert(`${x} is a valid Diners card number`);
+          
+        }
+        else if( jcb.test(x))
+        {
+            window.alert(`${x} is a valid JCB card number`);
+          
+        }
+        else if( maestro.test(x))
+        {
+            window.alert(`${x} is a valid Maestro card number`);
+          
         }
 
         else 
         { 
-            window.alert(" uknown card number.");
+            window.alert(" Uknown card number.");
          } 
 
-    if(amex.test(x))
-        {
-            window.alert(`${x} is a valid American Express number`);
-            
-        }
- else { window.alert("Invalid Aadhar no.");
+   
        }
       
-
+      
 
 
 
